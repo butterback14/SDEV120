3800f28f19581d342a15c76f3f6bbe3fa169ae40
Brayden Utterback <butterback14@ivytech.edu>
2025-04-25 15:11:22 -0400

    Add files via upload

---
+++ Module 6 Fundamental concepts of Python - IDLE brayden utterback.py
@@ -0,0 +1,13 @@
+# Variable c holds the word "cats"
+c = "cats"
+
+# Variable d holds the word "dogs"
+d = "dogs"
+
+# Variable s holds the complete sentence we want to print
+s = "It is raining " + c + " and " + d + "."
+
+# Print the sentence to the console
+print(s)
+
+
